8b58f947d5eccd1c65270ee304a6131f18b0cfe2
theivycoder <amber.emeny@griffithuni.edu.au>
2022-01-13 18:45:29 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { red } from '@mui/material/colors';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const theme = createTheme({
+    typography: {
+        fontFamily: [
+            'Roboto',
+            'Helvetica',
+            'Arial'
+        ]
+    },
+    palette: {
+        primary: {
+            main: red[500],
+        },
+    },
+});
+
+const App = () => {
+    return <ThemeProvider theme={theme}>...</ThemeProvider>
 }
 
 export default App;
